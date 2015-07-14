cb2229fcec17859c51b3c754c8fb4b36aa45a56d
Jackson1903 <randallcerdasjckson@gmail.com>
2015-07-13 21:23:08 -0600

    First commit

--- TeamChallenge/ViewController.swift
+++ TeamChallenge/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textFieldUsername: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var labelMessage: UILabel!
+   
+    @IBAction func verifyLogin(sender: AnyObject) {
+       
+       var username = 'Jackson'
+       var password = 'adm123'
+    
+       if textFieldUsername.text == username &&
+            textFieldPassword.text == password
+       {
+        println("Credential were correct")
+        }
+        else
+       {
+        println("Credential were incorrect")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
